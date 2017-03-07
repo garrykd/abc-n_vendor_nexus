d55cbb05d3fbde75671622deb1fb12974a349233
ezio84 <brabus84@gmail.com>
2017-03-07 10:24:24 +0100

    Switch to the new Substratum ThemeInterfacer [2/2]

--- configs/pure_main.mk
+++ configs/pure_main.mk
@@ -24,7 +24,7 @@ PRODUCT_PACKAGES += \
 # Custom Packages
 PRODUCT_PACKAGES += \
     Busybox \
-    masquerade
+    ThemeInterfacer
 
 # Include explicitly to work around GMS issues
 PRODUCT_PACKAGES += \
